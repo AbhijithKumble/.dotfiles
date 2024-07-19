fe43ff0c54cf8391e242a9546c758b7f9ff7f748
AbhijithKumble <abhijithkumble87564@gmail.com>
2024-07-19 12:05:33 +0530

    replace i3 status bar with polybar

--- nvim/.config/nvim/lua/abhijith/lazy/lsp.lua
+++ nvim/.config/nvim/lua/abhijith/lazy/lsp.lua
@@ -31,6 +31,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "clangd",
+                "gopls"
             },
             handlers = {
                 function(server_name) -- default handler (optional)
