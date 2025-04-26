8a0eae3174d952a598f0fa28a38650c34ea2e47b
AbhijithKumble <abhijithkumble87564@gmail.com>
2025-04-26 19:06:42 +0530

    new changes in themes,colors,status line in nvim, and fonts

--- nvim/.config/nvim/lua/abhijith/remap.lua
+++ nvim/.config/nvim/lua/abhijith/remap.lua
@@ -2,12 +2,12 @@ vim.g.mapleader = " "
 --go outside of editor to the directory
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
---move the selected line above or below 
+--move the selected line above or below
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
---append next line to current line 
+--append next line to current line
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -22,22 +22,22 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 -- copy into the system register
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 -- copy the whole line into the system register
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- delete line without overwriting the clipboard
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 
 
--- learn it 
+-- learn it
 --vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- try to use it 
+-- try to use it
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
@@ -50,4 +50,3 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
